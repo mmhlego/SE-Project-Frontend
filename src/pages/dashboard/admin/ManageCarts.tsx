cd11a990d78f28cc13cc77d73b4c5242f22d4c49
mmhlego <mmhlego@gmail.com>
2023-01-25 08:14:20 +0330

    Added placeholder text to ManageCarts

--- src/pages/dashboard/admin/ManageCarts.tsx
+++ src/pages/dashboard/admin/ManageCarts.tsx
@@ -1,3 +1,7 @@
 export default function ManageCarts() {
-	return <div>ManageCarts</div>;
+	return (
+		<div className="w-full h-full p-8 flex items-center justify-center font-medium text-xl">
+			<p>سبد خریدی یافت نشد.</p>
+		</div>
+	);
 }
